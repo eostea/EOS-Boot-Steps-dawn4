e4726cb71692f742f61546fa8970a4f102667de6
lome <1209518758@qq.com>
2018-05-24 18:06:38 +0800

    fix

--- 04_TOKEN_CREATE_ISSUE.sh
+++ 04_TOKEN_CREATE_ISSUE.sh
@@ -8,5 +8,5 @@
 #
 ################################################################################
 
-./cleos.sh push action eosio.token create '["eosio", "2500000000.0000 SYS", 0, 0, 0]' -p eosio.token
+./cleos.sh push action eosio.token create '["eosio", "1000000000.0000 SYS", 0, 0, 0]' -p eosio.token
 ./cleos.sh push action eosio.token issue '["eosio",  "1000000000.0000 SYS", "init"]' -p eosio
