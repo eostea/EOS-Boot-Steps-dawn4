3aa5ea1ad9afed24ce0076b3827ea211ec806497
lome <1209518758@qq.com>
2018-07-20 14:55:48 +0800

    add

--- BIOS/7_INIT_ACCOUNTS_TRANSFER.sh
+++ BIOS/7_INIT_ACCOUNTS_TRANSFER.sh
@@ -2,4 +2,4 @@
 
 INIT_ACCOUNT="$( jq -r '.INIT_ACCOUNT' "00_CONFIG.conf" )"
 
-./cleos.sh transfer eosio $INIT_ACCOUNT  "150000001.0000 SYS" "init"
+./cleos.sh transfer eosio $INIT_ACCOUNT  "150000001.0000 EOS" "init"
--- BP/3_VOTE_PRODUCER.sh
+++ BP/3_VOTE_PRODUCER.sh
@@ -3,4 +3,5 @@
 INIT_ACCOUNT="$( jq -r '.INIT_ACCOUNT' "00_CONFIG.conf" )"
 
 ./cleos.sh system voteproducer prods $INIT_ACCOUNT $INIT_ACCOUNT -p $INIT_ACCOUNT
-./cleos.sh system claimrewards $INIT_ACCOUNT  //块奖励
+
+#./cleos.sh system claimrewards $INIT_ACCOUNT  //块奖励
